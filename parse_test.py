156ef281891bda1bbb704798576e3853343d4e62
ropbastos <ro.p.bastos@gmail.com>
2020-11-17 14:13:04 -0300

    formatacaozinha == nada

--- parse_test.py
+++ parse_test.py
@@ -2,15 +2,30 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Backpropagation Neural Network.')
 parser.add_argument('network', metavar='network', type=str,
-                   help='Arquivo indicando a estrutura da rede. A primeira linha do arquivo deve armazenar o fator de regularização λ a ser utilizado. Cada linha subsequente no arquivo representa uma camada e o valor numérico armazenado naquela linha indica o número de neurônios na camada correspondente. Note que a primeira camada corresponde à camada de entrada da rede e a última camada corresponde à camada de saídas.')
+                   help='Arquivo indicando a estrutura da rede. A primeira linha do \
+                   arquivo deve armazenar o fator de regularização λ a ser utilizado. \
+                   Cada linha subsequente no arquivo representa uma camada e o valor \
+                   numérico armazenado naquela linha indica o número de neurônios na \
+                   camada correspondente. Note que a primeira camada corresponde à \
+                   camada de entrada da rede e a última camada corresponde à camada \
+                   de saídas.')
 parser.add_argument('initial_weights', metavar='weights', type=str,
-                   help=' Arquivo indicando os pesos iniciais a serem utilizados pela rede. Cada linha do arquivo armazena os pesos iniciais dos neurônios de uma dada camada—isto é, os pesos dos neurônios da camada i são armazenados na linha i do arquivo. Os pesos de cada neurônio devem ser separados por vírgulas, e os neurônios devem ser separados por ponto-e-vírgula. Note que o primeiro peso de cada neurônio corresponde ao peso de bias.')
+                   help=' Arquivo indicando os pesos iniciais a serem utilizados pela \
+                   rede. Cada linha do arquivo armazena os pesos iniciais dos neurônios \
+                   de uma dada camada—isto é, os pesos dos neurônios da camada i são \
+                   armazenados na linha i do arquivo. Os pesos de cada neurônio devem \
+                   ser separados por vírgulas, e os neurônios devem ser separados por \
+                   ponto-e-vírgula. Note que o primeiro peso de cada neurônio corresponde \
+                   ao peso de bias.')
 parser.add_argument('dataset', metavar='data', type=str,
-                   help='Arquivo com um conjunto de treinamento. Cada linha representa uma instância e cada coluna representa um atributo. Após listados todos os valores dos atributos, há um ponto-e-vírgula, e os demais valores na linha indicam as saídas da rede para aquela instância.')
+                   help='Arquivo com um conjunto de treinamento. Cada linha representa \
+                   uma instância e cada coluna representa um atributo. Após listados \
+                   todos os valores dos atributos, há um ponto-e-vírgula, e os demais \
+                   valores na linha indicam as saídas da rede para aquela instância.')
 
 args = parser.parse_args()
 
-# Parse network description file.
+# Faz o parse do arquivo descritor da rede..
 with open(args.network) as network_file:
   network = [float(i) for i in network_file.read().splitlines()]
 
@@ -19,7 +34,7 @@ lamb = network.pop(0)
 print(f'lamb: {lamb}')
 print(f'network: {network}')
 
-# Parse initial weights file.
+# Faz o parse do arquivo que contém os pesos iniciais.
 initial_weights = []
 with open(args.initial_weights) as weights_file:
   line = weights_file.readline()
@@ -29,5 +44,5 @@ with open(args.initial_weights) as weights_file:
 
 print(f'initial_weights: {initial_weights}')
 
-# Obtain dataset.
+# Obtém o arquivo contendo o dataset.
 dataset = open(args.dataset)
